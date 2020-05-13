2c12fc443a8fdb4bcfffa0b549401a7bed0be81f
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-05-13 16:42:54 +0700

    re-locate css import

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,7 +8,6 @@ class Dashboard extends CI_Controller {
         // Import CSS, JS, Fonts
         $data['main'] = $this->load->view('include/main', NULL, TRUE);
         $data['navbar'] = $this->load->view('include/navbar', NULL, TRUE);
-        $data['assets'] = $this->load->view('assets-import/dashboard-siswa', NULL, TRUE);
         
         $this->load->view('page/dashboard-siswa',$data);
 	}
--- application/views/page/dashboard-siswa.php
+++ application/views/page/dashboard-siswa.php
@@ -2,7 +2,21 @@
 <head>
 	<title>Test</title>
 	<?php echo $main; ?>
-	<?php echo $assets; ?>
+	<!-- Import CSS -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/navbar.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/dashboard.css'); ?>">
+
+	<!-- Dropdown script -->
+	<script>
+		$(document).ready(function () {
+			$(".ui.toggle.button").click(function () {
+				$(".mobile.only.grid .ui.vertical.menu").toggle(100);
+			});
+
+			$(".ui.dropdown").dropdown();
+		});
+	</script>
+	<!-- End Dropdown script -->
 </head>
 <body>
 	<?php echo $navbar; ?>
