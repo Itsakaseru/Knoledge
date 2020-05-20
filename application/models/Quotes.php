419b4450aaf81fb686c4c4a20e80856e68f847f5
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-05-21 00:31:46 +0700

    random quote logic

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -5,14 +5,42 @@ class Quotes extends CI_Model{
 
     public function getQuote()
     {
+        $num = rand (0,29);
         // QOTD ( Quotes of the day )
         $quotes = array(
-            "pull mostima oi jangan lupa",
-            "Testing2",
-            "Testing3"
+            "don't play arknights too long",
+            "hehe boii",
+            "Testing3",
+            "Testing4",
+            "Testing5",
+            "Testing6",
+            "Testing7",
+            "Testing8",
+            "Testing9",
+            "Testing10",
+            "Testing11",
+            "Testing12",
+            "Testing13",
+            "Testing14",
+            "Testing15",
+            "Testing16",
+            "Testing17",
+            "Testing18",
+            "Testing19",
+            "Testing20",
+            "Testing21",
+            "Testing22",
+            "Testing23",
+            "Testing24",
+            "Testing25",
+            "Testing26",
+            "Testing27",
+            "Testing28",
+            "Testing29",
+            "Testing30",
         );
 
-        return $quotes[0];
+        return $quotes[$num];
     }
 
 }
