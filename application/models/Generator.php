7a771008a749844db44969cf852dfb1b39e94922
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-05-25 19:44:42 +0700

    add string generator model

---
+++ application/models/Generator.php
@@ -0,0 +1,21 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed !');
+
+class Generator extends CI_Model{
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getSalt()
+    {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        $result = '';
+        for ($i = 0; $i < 5; $i++) $result .= $characters[mt_rand(0, 61)];
+
+        return $result;
+    }
+
+}
+?>
