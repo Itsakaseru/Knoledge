c14627289000bbcf5c805756bc1a9572d29aba49
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-05-30 15:34:51 +0700

    add custom 404 page

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,81 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
 <style type="text/css">
+html {
+	font-family: 'Myriad Pro Regular' !important;
+}
 
+.for-container {
+    width: 100%;  
+    min-height: 100vh;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    position: fixed; 
+    overflow-y: scroll;
+}
+
+.for-container img{
+    width: 100vh;  
+    height: auto;
+}
+
+.btnCenter {
+    display: flex;
+}
+
+.btn {
+  font-weight: 400;
+  padding: 20px;
+  background-color: #5e7fdc;
+  color: white;
+  width: 320px;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 1.2em;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  transition: all 0.2s linear;
+}
+@media screen and (max-width: 400px) {
+  .btn {
+    margin: 0 auto;
+    margin-top: 60px;
+    margin-bottom: 50px;
+    width: 200px;
+  }
+}
+.btn:hover {
+  background-color: rgba(94, 127, 220, 0.8);
+  transition: all 0.2s linear;
+}
 ::selection { background-color: #E13300; color: white; }
 ::-moz-selection { background-color: #E13300; color: white; }
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
 </style>
+<head>
+<meta charset="utf-8">
+<title>404 Page Not Found</title>
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+<div class="for-container" style="overflow: hidden;">
+    <div class="left">
+        <h1 class="text-center">Oops! Something went wrong!</h1>
+        <h4 style="text-align: center; text-decoration: none !important;">404 Not Found</h4>
+        <div class="btnCenter">
+            <a style="text-align: center; text-decoration: none !important;" class="btn" href="<?php echo config_item('base_url') . 'Dashboard'; ?>">Return to Dashboard</a>
+        </div>
+    </div>
+    <div class="right">
+        <img src="<?php echo config_item('base_url') . 'assets/images/404.jpg'; ?>" alt=":3"/>
+    </div>
+</div>
 </body>
 </html>
