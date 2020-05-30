b23692e9852d92edf0807c7ab6d072835f6cc265
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-05-30 17:29:28 +0700

    update motds, quotes number reduce to 5

--- application/models/Motd.php
+++ application/models/Motd.php
@@ -5,39 +5,19 @@ class Motd extends CI_Model{
 
     public function getMotd()
     {
-        $num = rand (0,29);
+        $num = rand (0,9);
         // QOTD ( Quotes of the day )
         $motd = array(
             "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too longdon't play arknights too long",
-            "don't play arknights too long",
-            "don't play arknights too long",
+            "playing is fun isn't?",
+            "respect everyone",
+            "learning is learning",
+            "don't forget your homework",
+            "don't forget to eat",
+            "ain't nobody got time for that",
+            "welcome to your dashboard",
+            "have a great day",
+            "why are we still here..."
         );
 
         return $motd[$num];
