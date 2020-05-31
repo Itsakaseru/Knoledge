c9f56f2a641f8dba15d7f806c64c1776840825e9
Itsakaseru <32617101+Itsakaseru@users.noreply.github.com>
2020-05-31 12:44:37 +0700

    add more quotes

--- application/models/Quote.php
+++ application/models/Quote.php
@@ -8,11 +8,11 @@ class Quote extends CI_Model{
         $num = rand (0,4);
         // QOTD ( Quotes of the day )
         $quotes = array(
-            "Practice makes us right,</br> repetitions make us perfect.",
-            "Give a man a program,</br> frustrate him for a day.</br> Teach a man to program,</br> frustrate him for a lifetime.",
-            "Testing3",
-            "Testing4",
-            "Testing5"
+            "Practice makes us right,</br>repetitions make us perfect.",
+            "Give a man a program,</br>frustrate him for a day.</br>Teach a man to program,</br>frustrate him for a lifetime.",
+            "If at first you don't success </br>try, try again.",
+            "Do what you can, with what you have,</br>where you are.",
+            "If opportunity doesn’t knock,</br>build a door."
         );
 
         return $quotes[$num];
